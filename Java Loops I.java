17d367ca784e65cca9c8271c94d5d316dd29cec4
Sambhav Thakur <sambhavkumar228@gmail.com>
2020-05-10 21:54:25 +0530

    Create Java Loops I.java

---
+++ Java Loops I.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);    
+        int N = scanner.nextInt();
+        for(int i=1;i<=10;i++)
+        {
+            int a=N*i;
+        System.out.println(+N+" x " +i+ " = " +a);
+
+        }
+        scanner.close();
+    }
+}
