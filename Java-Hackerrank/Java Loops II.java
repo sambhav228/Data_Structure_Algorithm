f77ba7a671d6251c300ed17e4ab5f7f2d1c425ae
Sambhav Thakur <sambhavkumar228@gmail.com>
2020-05-12 09:56:06 +0530

    Create Java Loops II.java

---
+++ Java-Hackerrank/Java Loops II.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+        }
+        in.close();
+    }
+}
