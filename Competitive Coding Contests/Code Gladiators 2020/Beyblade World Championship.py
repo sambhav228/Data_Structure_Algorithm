48cdfd58572ccc4b5cb4e9424516fed24426d260
Sambhav Thakur <sambhavkumar228@gmail.com>
2020-05-23 09:20:41 +0530

    Add files via upload

---
+++ Competitive Coding Contests/Code Gladiators 2020/Beyblade World Championship.py
@@ -0,0 +1,18 @@
+def main():
+    num=int(input())
+    for i in range(num):
+        s=0
+        num1=int(input())
+        t1=list(map(int,input().split()))
+        t1.sort(reverse=True)
+        t2=list(map(int,input().split()))
+        t2.sort(reverse=True)
+        cnt=0
+        for i in range(num1):
+            for j in range(cnt,num1):
+                if t1[i]>t2[j] :
+                    cnt=j+1
+                    s+=1
+                    break
+        print(s)
+main()
---
+++ Competitive Coding Contests/Code Gladiators 2020/The Powerpuff Girls.cpp
@@ -0,0 +1,28 @@
+/*Enter your code here. Read input from STDIN. Print your output to STDOUT*/
+#include <iostream>
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	long n;
+	cin>>n;
+	long a[n],b[n],c[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+
+	}
+	for(int j=0;j<n;j++)
+	{
+		cin>>b[j];
+
+	}
+	for(int k=0;k<n;k++)
+	{
+		c[k]=b[k]/a[k];
+	}
+std::cout<< *std::min_element(c,c+n)<<endl ;
+
+
+}
