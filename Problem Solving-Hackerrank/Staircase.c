0064ab2606f29cb162732d09e8a5ec932a5cb685
Sambhav Thakur <sambhavkumar228@gmail.com>
2020-05-26 21:18:53 +0530

    Create Staircase.c

---
+++ Problem Solving-Hackerrank/Staircase.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{ int i,j,k,n;
+ scanf("%d",&n);
+ for(i=1;i<=n;i++)
+ {
+  for(j=1;j<n-i+1;j++)
+   {
+     printf(" ");
+   }
+  for(k=1;k<=i;k++)
+   {
+     printf("#");
+   }
+    printf("\n");
+ }
+    return 0;
+}
