64cd3bacb9bdddf650392a4510dbe5ccbd9fed0b
Sambhav Thakur <sambhavkumar228@gmail.com>
2020-07-29 22:27:34 +0530

    Add files via upload

---
+++ GeeksForGeeks/POSTlab 1-lab 1.py
@@ -0,0 +1,37 @@
+#POSTlab 1---lab 1
+#KLU_180031058_SAMBHAV KUMAR THAKUR
+
+def isVowel(ch) : 
+	
+	ch = ch.upper(); 
+	return (ch == 'A' or ch == 'E'or ch == 'I' or
+			ch == 'O'or ch == 'U'); 
+
+def countVowels(string) : 
+
+	count = 0; 
+	for i in range(len(string)) : 
+		
+		if (isVowel(string[i])) : 
+			count += 1; 
+			
+	return count; 
+
+def sortArr(arr, n) : 
+	
+	vp = []; 
+
+	for i in range(n) : 
+
+		vp.append((countVowels(arr[i]),arr[i])); 
+
+	vp.sort() 
+
+	for i in range(len(vp)) : 
+		print(vp[i][1], end= " "); 
+
+if __name__ == "__main__" : 
+	arr = [ "pointer", "array" ,"code", "for" ]; 
+	n = len(arr); 
+
+	sortArr(arr, n);
