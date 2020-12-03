a06851ef6452205bcd44593d91ead8c78d8e562f
Sambhav Thakur <sambhavkumar228@gmail.com>
2020-12-03 09:30:14 +0530

    Update Java Loops I.java

--- Java Loops I.java
+++ Java Loops I.java
@@ -1,5 +1,5 @@
 import java.io.*;
-import java.util.*;
+import java.util;
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);    
