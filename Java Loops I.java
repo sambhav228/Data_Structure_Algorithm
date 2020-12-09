55d4f9bd19b563caa3bfb6ce91e101255f9b3db5
Sambhav Thakur <sambhavkumar228@gmail.com>
2020-12-10 00:50:21 +0530

    Update Java Loops I.java

--- Java Loops I.java
+++ Java Loops I.java
@@ -6,7 +6,6 @@ public class Solution {
     {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
-
         for(int i = 1; i <= 10; i++){
 
             System.out.printf("%d x %d = %d%n", N, i, N*i);
